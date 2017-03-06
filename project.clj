94d3b281b48a5abec4d5d6514d50147484c73d62
Brandon Adams <emidln@gmail.com>
2017-03-06 16:44:43 -0600

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/CuriosityApp/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.amazonaws/aws-java-sdk "1.11.65"]
-                 [clj-time "0.12.2"]]
-  :plugins [[lein-codox "0.10.2"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [com.amazonaws/aws-java-sdk "1.11.98"]
+                 [clj-time "0.13.0"]]
+  :plugins [[lein-codox "0.10.3"]])
